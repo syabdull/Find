fa1800152410659089f320eca3fc0d67f6809ada
syabdull <syab5050@gmail.com>
2022-06-17 08:56:24 +0530

    function with parameter and return type

--- MODULE2/intro.js
+++ MODULE2/intro.js
@@ -28,3 +28,34 @@
 //                      console.log("given no is prime");
 //                 }
 //             }
+ // fucntion in js with out parameter
+    function sayHi(){
+         console.log("say hi");
+    }
+// calling funciotn 
+  sayHi();
+  // nextReaching to  function with paramete 
+  function sayHello(num1,num2){
+    console.log(num1+num2);
+
+  }
+  // calling paramaterized funciton 
+    sayHello(1,2);
+
+// function with return type 
+ function multiply(n1,n2){
+      return n1*n2;
+
+ }
+
+console.log(multiply(1,10));
+
+
+
+
+
+
+
+
+
+
