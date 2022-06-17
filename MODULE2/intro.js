56c5a106581106dbc9003434eca76869bdf8ede5
syabdull <syab5050@gmail.com>
2022-06-17 10:26:00 +0530

    error fixed

--- MODULE2/intro.js
+++ MODULE2/intro.js
@@ -1,59 +1,109 @@
-// console.log("swagat hai  aap ka");
-//  // variablese 
-//  let a =10;
-//   console.log(a);
-//   a="Abdulla";
-//   console.log(a);
-// //   // for loop and while loop
-// //     for(let i=0;i<=10;i++){
-// //        console.log(i);
-// //     }
-// // while loop 
-//      let z=10;
-//      while(z>=1){
-//         console.log(z);
-//         z--;
+// // console.log("swagat hai  aap ka");
+// //  // variablese 
+// //  let a =10;
+// //   console.log(a);
+// //   a="Abdulla";
+// //   console.log(a);
+// // //   // for loop and while loop
+// // //     for(let i=0;i<=10;i++){
+// // //        console.log(i);
+// // //     }
+// // // while loop 
+// //      let z=10;
+// //      while(z>=1){
+// //         console.log(z);
+// //         z--;
 
-//      }
-//      // java Script program to check weather given no is prime or no 
-//            let n=10;
-//             for(let i=2;i*i<=n;i++){
-//                 if(n<=1){
-//                     console.log("not a prime");
-//                 }
-//                  if(n%i==0){
-//                      console.log("not a prime");
-//                 }
-//                 else {
-//                      console.log("given no is prime");
-//                 }
-//             }
- // fucntion in js with out parameter
-    function sayHi(){
-         console.log("say hi");
-    }
-// calling funciotn 
-  sayHi();
-  // nextReaching to  function with paramete 
-  function sayHello(num1,num2){
-    console.log(num1+num2);
+// //      }
+// //      // java Script program to check weather given no is prime or no 
+// //            let n=10;
+// //             for(let i=2;i*i<=n;i++){
+// //                 if(n<=1){
+// //                     console.log("not a prime");
+// //                 }
+// //                  if(n%i==0){
+// //                      console.log("not a prime");
+// //                 }
+// //                 else {
+// //                      console.log("given no is prime");
+// //                 }
+// //             }
+//  // fucntion in js with out parameter
+//     function sayHi(){
+//          console.log("say hi");
+//     }
+// // calling funciotn 
+//   sayHi();
+//   // nextReaching to  function with paramete 
+//   function sayHello(num1,num2){
+//     console.log(num1+num2);
 
-  }
-  // calling paramaterized funciton 
-    sayHello(1,2);
+//   }
+//   // calling paramaterized funciton 
+//     sayHello(1,2);
 
-// function with return type 
- function multiply(n1,n2){
-      return n1*n2;
+// // function with return type 
+//  function multiply(n1,n2){
+//       return n1*n2;
 
- }
+//  }
 
-console.log(multiply(1,10));
+// console.log(multiply(1,10));
+
+// // iife function  immediately invoked function expression
+//   (function(){
+//     console.log("Hellow there");
+
+//   })();
+// // iife immediately invoked funciton expression
+//  (function(){
+//     console.log("hey bro");
+ 
+//  })(); // storing the funciton in a variable
+//  let c=function p(n){
+//     return a/10;
+
+//  }
+//  console.log(a(10));
+
+// 
+
+// Array with elements 
+let ele=["Abdulla",1,2,false,2,2];
+ console.log(ele);
 
 
-
-
-
+console.log(ele[3]);
+// Array's push method 
+let eleArr=["Abdulla","rehmath","Ashraf"];
+console.log('Array before push');
+ console.log(eleArr);
+ console.log("Array After push");
+ eleArr.push("Mehaboob");
+  console.log(eleArr);
+console.log("/////////////////////////////////////////////////////////////////");
+ eleArr.pop();
+  console.log(eleArr);
+  //All about  shift method 
+   eleArr.shift();
+   eleArr.shift();
+   console.log(eleArr);
+    eleArr.unshift("rehmath");
+    console.log(eleArr);
+    console.log("///////////////////////////////////done -----done---------done")
+console.log(eleArr.length);
+////starting with strings 
+let str="iam  feeling good now";
+console.log(str.length);
+ let newArr="A bdu lla";
+ let  updated=newArr.slice(2,4);
+  console.log(updated);
+  console.log(updated.replace("d","b"));
+  console.log(updated.toUpperCase());
+  console.log(updated.toLowerCase());
+  console.log(updated.concat(newArr));
+  console.log(newArr.charAt(3));
+  console.log(newArr.split(" "));
 
 
 
