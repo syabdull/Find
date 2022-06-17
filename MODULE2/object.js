a03a17754237f3f9d6e0f94a2ae2d2aef516a9fd
syabdull <syab5050@gmail.com>
2022-06-17 21:23:41 +0530

    js added

---
+++ MODULE2/object.js
@@ -0,0 +1,14 @@
+let obj={
+    Name:"Abdulla",
+    class:"two",
+     arr:["this is an array","This is also an array",2,4,true],
+    obj:{
+        name:"city",
+        city:"ANDRA"
+    },
+    sayHi:function(){
+        console.log("say hi");
+    }
+
+}
+console.log(obj);
